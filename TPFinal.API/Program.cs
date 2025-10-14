8c5728beed11ba0715d8fcc5dd630c346d822ed4
HakimMaghraoui <36701735+HakimMaghraoui@users.noreply.github.com>
2025-10-14 15:15:18 +0200

    client api get + create

--- TPFinal.API/Program.cs
+++ TPFinal.API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using TPFinal.Business.Abstractions;
+using TPFinal.Business.Services;
 using TPFinal.DAL.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,26 +14,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TPFinalDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("TPFinalDbContext")));
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Version = "v1",
-        Title = "ConsultTech API",
-        Description = "API de gestion des consultants, missions, clients et compétences"
-    });
-});
+builder.Services.AddScoped<IClientService, ClientService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ConsultTech API v1");
-        options.RoutePrefix = string.Empty; // Swagger à la racine
-    });
+    app.UseSwaggerUI();
 
 }
 
