813dbbae0a2fa2b4e5af024b4ec14a4f1ed18cc0
Guillaume <guillaume.mene@gmail.com>
2025-10-14 16:01:05 +0200

    g-MissionControllerAPI

--- TPFinal.API/Controllers/MissionsController.cs
+++ TPFinal.API/Controllers/MissionsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TPFinal.Business.Abstractions;
+using TPFinal.Business.Models;
+using TPFinal.Business.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,5 +11,54 @@ namespace TPFinal.API.Controllers;
 [ApiController]
 public class MissionsController : ControllerBase
 {
-    
+    private readonly IMissionService _missionService;
+    public MissionsController(IMissionService missionService)
+    {
+        _missionService = missionService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllMissions()
+    {
+        var missions = _missionService.GetAllMissions();
+        return Ok(missions);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGetMissionById(Guid id)
+    {
+        var mission = _missionService.GetMissionById(id);
+        if (mission == null)
+            return NotFound();
+
+        return Ok(mission);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateMission([FromBody] MissionDTO missionDto)
+    {
+        var missionId = await _missionService.CreateMissionAsync(missionDto);
+        return CreatedAtAction(nameof(GetAllMissions), new { id = missionId }, missionId);
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateMission(Guid id, [FromBody] MissionDTO missionDto)
+    {
+        var updatedMission = await _missionService.UpdateMissionAsync(id, missionDto);
+        if (updatedMission == null)
+            return NotFound();
+
+        return Ok(updatedMission);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMission(Guid id)
+    {
+        var deleted = await _missionService.DeleteMissionAsync(id);
+        if (!deleted)
+            return NotFound();
+
+        return NoContent();
+    }
+    [HttpGet("exists/{id}")]
+    public async Task<IActionResult> MissionExists(Guid id)
+    {
+        var exists = await _missionService.MissionExistAsync(id);
+        return Ok(exists);
+    }
 }
--- TPFinal.Business/Models/MissionDTO.cs
+++ TPFinal.Business/Models/MissionDTO.cs
@@ -1,6 +1,33 @@
-﻿namespace TPFinal.Business.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using TPFinal.Business.Abstractions;
+
+namespace TPFinal.Business.Models;
 
 public class MissionDTO
 {
+    public Guid Id { get; set; }
 
+    [Required(ErrorMessage = "Le titre est obligatoire.")]
+    [StringLength(50, ErrorMessage = "Le titre ne peut pas dépasser 50 caractères.")]
+    public string Titre { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La description est obligatoire.")]
+    public string Description { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La date de début est obligatoire.")]
+    [DataType(DataType.Date, ErrorMessage = "La date de début doit être une date valide.")]
+    public DateTime DateDebut { get; set; }
+
+    [Required(ErrorMessage = "La date de fin est obligatoire.")]
+    [DataType(DataType.Date, ErrorMessage = "La date de fin doit être une date valide.")]
+    public DateTime DateFin { get; set; }
+
+    [Required(ErrorMessage = "Le budget est obligatoire.")]
+    [Range(0.1, double.MaxValue, ErrorMessage = "Le budget doit être un nombre positif.")]
+    public decimal Budget { get; set; }
+
+    [Required(ErrorMessage = "Le client est obligatoire.")]
+    public Guid ClientId { get; set; }
+
+    public Guid? ConsultantId { get; set; }
 }
