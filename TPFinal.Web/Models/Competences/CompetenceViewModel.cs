bc53b4d69ffeffa68d288e71b2eceefe590064b3
HakimMaghraoui <36701735+HakimMaghraoui@users.noreply.github.com>
2025-10-15 12:51:01 +0200

    Consultants MVC

---
+++ TPFinal.Web/Models/Competences/CompetenceViewModel.cs
@@ -0,0 +1,15 @@
+﻿// TPFinal.Web/Models/CompetenceViewModel.cs
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class CompetenceViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Compétence technique est obligatoire")]
+    public string CompetenceTechnique { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Catégorie est obligatoire")]
+    [StringLength(50, ErrorMessage = "La catégorie ne peut pas dépasser 50 caractères.")]
+    public string Categorie { get; set; } = string.Empty;
+}
